385daf3050cebc8a820a814bd5c8df291e535f37
Andre5123 <andre_hoe@yahoo.ca>
2026-07-04 18:45:21 -0400

    Now using MobileNetV2 as a backbone for transfer learning

--- src/models/detector.py
+++ src/models/detector.py
@@ -12,7 +12,24 @@ def conv_block(x, n_filters, l2=0.001):
     output = layers.MaxPool2D(pool_size=(2,2))(x)
     return output
 
-def build_detector(input_shape=(256,256, 3)):
+
+
+def build_detector(input_shape=(128, 128, 3)):
+    backbone = tf.keras.applications.MobileNetV2(
+        input_shape=input_shape,
+        include_top=False,
+        weights='imagenet'
+    )
+    backbone.trainable = False  # freeze pretrained weights initially
+    
+    inputs = keras.Input(shape=input_shape)
+    x = backbone(inputs, training=False)
+    outputs = layers.Conv2D(5, 1, activation=None)(x)
+    
+    return keras.Model(inputs=inputs, outputs=outputs, name="detector_model")
+
+#Deprecated
+def build_detector_old(input_shape=(256,256, 3)):
     image_inputs = keras.Input(shape=input_shape)
     x = conv_block(image_inputs, 16)
     x = conv_block(x, 32)
